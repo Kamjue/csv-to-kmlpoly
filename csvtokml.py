bfc85c63a573c3c0bfd4b8255f531a1be8ce8d88
Daniel Lobo <Kamjue@users.noreply.github.com>
2021-05-24 20:16:09 -0400

    First upload

---
+++ csvtokml.py
@@ -0,0 +1,31 @@
+import sys
+import csv
+
+print("\n\t** Coordenada a Polígono KML **\n")
+
+if( len(sys.argv) == 2 and sys.argv[1].endswith(".csv") ):
+	with open( sys.argv[1], newline="" ) as csvfile:
+		csvreader = csv.reader(csvfile, delimiter=";")
+		coords = []
+		rowNew = []
+		coordsStr = ""
+
+		for row in csvreader:
+			rowNew.append(row)
+
+		for row in rowNew[1:]:
+			aux = row[2] + "," + row[1]
+			coords.append(aux)
+
+		#print(coords)
+		coordsStr = "\n".join(coords)
+
+		with open("output.kml", "w") as kmlfile:
+			pname = input("Nombre del polígono: ")
+
+			kmlfile.write("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<kml xmlns=\"http://www.opengis.net/kml/2.2\">\n\t<Placemark>\n\t\t<name>" + pname + "</name>\n\t\t<Polygon>\n\t\t\t<extrude>1</extrude>\n\t\t\t<altitudeMode>relativeToGround</altitudeMode>\n\t\t\t<outerBoundaryIs>\n\t\t\t\t<LinearRing>\n\t\t\t\t\t<coordinates>\n" + coordsStr + "\n\t\t\t\t\t</coordinates>\n\t\t\t\t</LinearRing>\n\t\t\t</outerBoundaryIs>\n\t\t</Polygon>\n\t</Placemark>\n</kml>")
+
+else:
+	print("\t--** Falta archivo csv a cargar **--\n")
+	# file = open("coord.kml");
+	sys.exit()
